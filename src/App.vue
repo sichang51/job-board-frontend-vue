555e2723415af7ffdc51110305d7c3a5133d7d16
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-11 21:41:28 -0500

    adds script for create action

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,49 @@ export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
+      newJobParams: {},
     };
   },
   created: function () {},
   methods: {},
+
+  createJob: function () {
+    axios
+      .post("/jobs.json", this.newJobParams)
+      .then((response) => {
+        console.log("jobs create", response);
+        this.jobs.push(response.data);
+        this.newJobParams = {};
+      })
+      .catch((error) => {
+        console.log("jobs create error", error.response);
+      });
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h1>New Jobs</h1>
+    <div>
+      Company Id:
+      <input />
+      Company Id:
+      <input />
+      Company Id:
+      <input />
+      Company Id:
+      <input />
+      Company Id:
+      <input />
+      Company Id:
+      <input />
+      Company Id:
+      <input />
+    </div>
   </div>
 </template>
 
 <style></style>
+company_id, title, description, url, location, active, salary_range
